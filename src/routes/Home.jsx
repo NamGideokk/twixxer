848b444e1a2947779a8bc361990c90975b72da67
Nam Gideok <namgd1222@gmail.com>
2022-04-18 14:14:21 +0900

    피드 폼 css 수정

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,6 +3,41 @@ import Footer from "components/Footer";
 import Auth from "./Auth";
 import Navigation from "components/Navigation";
 import { useAuth } from "myFirebase";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
+
+const FormStyle = styled.div`
+  .feed__form {
+    width: 800px;
+    height: fit-content;
+    padding: 20px;
+    margin: 100px auto;
+
+    input {
+      width: 100%;
+      height: 50px;
+      padding: 0 20px;
+      border: none;
+      border-radius: 10px;
+      font-size: 24px;
+      transition: 0.3s;
+
+      :focus {
+        box-shadow: 0 2px 10px var(--logo-color);
+      }
+    }
+
+    button {
+      font-size: 30px;
+      padding: 5px;
+      background-color: transparent;
+      color: var(--logo-color);
+      position: absolute;
+      transform: translate(-50px, 5px);
+    }
+  }
+`;
 
 const Home = () => {
   const currentUser = useAuth();
@@ -10,6 +45,12 @@ const Home = () => {
 
   function onSubmit(e) {
     e.preventDefault();
+    if (feed.length === 0) {
+      alert("내용을 입력해 주세요.");
+    } else {
+      alert("피드 작성!");
+      setFeed("");
+    }
   }
   function onChange(e) {
     const {
@@ -23,16 +64,23 @@ const Home = () => {
       {currentUser ? (
         <>
           <Navigation />
-          <form onSubmit={onSubmit}>
-            <input
-              type="text"
-              placeholder="내용을 입력하세요"
-              maxLength={120}
-              value={feed}
-              onChange={onChange}
-            />
-            <input type="submit" value="트윗" />
-          </form>
+          <FormStyle>
+            <form onSubmit={onSubmit} className="feed__form">
+              <input
+                type="text"
+                placeholder="친구들과 소식을 공유하세요!"
+                maxLength={120}
+                value={feed}
+                onChange={onChange}
+              />
+              <button type="submit" className="upload-feed__button">
+                <FontAwesomeIcon
+                  icon={faPaperPlane}
+                  className="upload-feed__icon"
+                />
+              </button>
+            </form>
+          </FormStyle>
           <Footer />
         </>
       ) : (
