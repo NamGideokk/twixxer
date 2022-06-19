2dbf3c87090badc581a8873b09105e8bae44cb53
Nam Gideok <namgd1222@gmail.com>
2022-06-19 21:03:29 +0900

    리팩토링 > scss 설치, css 변수 분리, EditModal

---
+++ src/common/EditModal/EditModal.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./EditModal.scss";
+
+const EditModal = ({ animation }) => {
+  return (
+    <div className="wrapper-style">
+      <div className={`edit__container ${animation}`}>
+        <textarea
+          className="prev-content"
+          value={editContent}
+          onChange={handleEditContent}
+        />
+        <button onClick={editConfirm}>수정하기</button>
+        <button onClick={editCancel}>취소</button>
+      </div>
+    </div>
+  );
+};
+
+export default EditModal;
