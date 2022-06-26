c937c7114b06bd562e3f342ce94fdad7d0ed80eb
Nam Gideok <namgd1222@gmail.com>
2022-06-26 12:30:59 +0900

    Bookmark > 리팩토링, scss, 헤더 컴포넌트 분리

---
+++ src/components/BookmarkHeader.jsx/BookmarkHeader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./BookmarkHeader.scss";
+
+const BookmarkHeader = () => {
+  return (
+    <header className="bookmark__header">
+      북마크
+      <small>Bookmarks</small>
+    </header>
+  );
+};
+
+export default BookmarkHeader;
